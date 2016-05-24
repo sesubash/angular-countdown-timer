2807609940bd5e60da81cac6ec8f2fd1896fc536
Subash <sselvara@adobe.com>
2016-05-24 13:07:40 +0530

    updated duration in demo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 angular.module("app", ['sesu.countdown'])
 .controller("mainController", function($scope){
-	$scope.time = 0;
+	$scope.time = 1000;
 	$scope.finished = function(){
 		console.log("finished");
 	};
